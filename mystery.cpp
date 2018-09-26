c93430192f9d5cb697aa1899d9105586cfccd6ac
kyson1 <kyson1@user.noreply.com>
2018-09-26 14:21:48 -0400

    Lab 2

--- mystery.cpp
+++ mystery.cpp
@@ -10,7 +10,7 @@ void print(auto A)
    cout<<endl;
 }
 
-void mystery1(auto& D)
+void mystery1(auto& Data)
 {
   cout<<endl<<"Mystery 1"<<endl<<"---------------------"<<endl;
 
@@ -20,12 +20,41 @@ void mystery1(auto& D)
 	if ( Data[ i ] < Data[ j ] )
 	    swap( Data[ i ] , Data[ j ] );
 
-    print(D);
+    print(Data);
   }//end outer for (this brace is needed to include the print statement)
 
 }
 
-//... Other mysteries...
+void mystery2(auto& Data)
+{
+  cout<<endl<<"Mystery 2"<<endl<<"---------------------"<<endl;
+
+  for ( int i = 0 ; i < Data.size( ) ; i++)
+  {
+    for ( int j = 0 ; j < Data.size() -1 ; j++)
+        if ( Data[ j+1 ] < Data[ j ] )
+            swap( Data[ j +1 ] , Data[ j ] );
+
+    print(Data);
+  }//end outer for (this brace is needed to include the print statement)
+
+}
+
+void mystery3(auto& Data)
+{
+  cout<<endl<<"Mystery 3"<<endl<<"---------------------"<<endl;
+
+  for ( int i = 0 ; i < Data.size( ) ; i++)
+  {
+    for ( int j = i ; j < Data.size ; j++)
+        if ( Data[ j ] < Data[ i ] )
+            swap( Data[ i ] , Data[ j ] );
+
+    print(Data);
+  }//end outer for (this brace is needed to include the print statement)
+
+}
+
 
 int main()
 {
